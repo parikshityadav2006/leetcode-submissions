bf5730382f7f7b1c780106e7217d883999368f2d
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-06-27 23:09:44 +0530

    Add solution for Reorder List

---
+++ solutions/medium/Reorder List.cpp
@@ -0,0 +1,32 @@
+// Title: Reorder List
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/reorder-list/
+
+        scout=head;
+        }
+        
+        int half=ceil(count/2.0);
+        while(scout->next!=nullptr){
+            scout=scout->next;
+            count++;
+        int count=1;
+        
+        ListNode* scout=head;
+        
+public:
+    void reorderList(ListNode* head) {
+class Solution {
+ */
+ * };
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode *next;
+
+        if(head->next==nullptr || head->next->next==nullptr){
+            return;
+        }
+        count=1;
+        
+        while(count!=half){
