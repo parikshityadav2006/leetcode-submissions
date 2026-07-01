9855414471022d0ced582adfdc3565896b162a9b
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-07-01 13:29:14 +0530

    Add solution for LRU Cache

--- solutions/medium/LRU Cache.cpp
+++ solutions/medium/LRU Cache.cpp
@@ -3,9 +3,33 @@
             // Language: C++
             // Link: https://leetcode.com/problems/lru-cache/
 
-                }
-                tail->next=new Node(key,value);
-                tail->next->prev=tail;
-                tail=tail->next;
-                mp[key]=tail;
-                tail->next=nullptr;
+        if (cache.find(key) == cache.end()) return -1;
+    int get(int key) {
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+
+
+public:
+        order.erase(cache[key].second);
+        order.push_back(key);
+        cache[key].second = --order.end();
+        return cache[key].first;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            order.erase(cache[key].second);
+        } else if (cache.size() == capacity) {
+            int lru = order.front();
+            order.pop_front();
+            cache.erase(lru);
+        }
+        order.push_back(key);
+        cache[key] = {value, --order.end()};
+    }
+    int capacity;
+    list<int> order;
+class LRUCache {
+private:
+    unordered_map<int, pair<int, list<int>::iterator>> cache;
