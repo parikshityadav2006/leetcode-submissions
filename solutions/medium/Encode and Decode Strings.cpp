14ea7d4404c211c28218ea6d544988540548b68a
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-07-02 13:44:53 +0530

    Add solution for Encode and Decode Strings

---
+++ solutions/medium/Encode and Decode Strings.cpp
@@ -0,0 +1,24 @@
+// Title: Encode and Decode Strings
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/encode-and-decode-strings/
+
+        string len = "";
+
+        vector<string> strs;
+    vector<string> decode(string s) {
+
+        return s;
+    }
+        }
+
+            temp.clear();
+            s = s + temp;
+            string temp = to_string(len) + "/" + str;
+
+            int len = str.size();
+        string s = "";
+        for (string str : strs) {
+    string encode(vector<string>& strs) {
+   public:
+class Codec {
