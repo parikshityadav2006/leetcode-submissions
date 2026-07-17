e077366397e629e856db0d1f585299daca41bf6b
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-07-17 17:09:12 +0530

    Add solution for Implement Trie (Prefix Tree)

--- solutions/medium/Implement Trie (Prefix Tree).cpp
+++ solutions/medium/Implement Trie (Prefix Tree).cpp
@@ -3,35 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/implement-trie-(prefix-tree)/
 
-            int idx=c-'a';
-        for(char c : word){
-        TrieNode* node=root;
-    bool search(string word) {
-    
-    }
-        }
-        node->isEndofWord=true;
-            }
-            node=node->child[idx];
-                node->child[idx]=new TrieNode();
-            if (!node->child[idx]){
-            int idx=c-'a';
-        for(char c : word){
-        TrieNode* node=root;
-    
-    void insert(string word) {
-        root= new TrieNode();
-    }
-public:
-    Trie() {
-class Trie {
-private:
-    TrieNode* root;
-    TrieNode() {}
-};
-
-    TrieNode* child[26]={nullptr};
-    
-class TrieNode{
-public:
-    bool isEndofWord=false;
+ 
