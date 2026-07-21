027386ed0c4a8bb78c6ac9eb7c7011b380e590bc
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-07-21 13:53:41 +0530

    Add solution for Permutations

--- solutions/medium/Permutations.cpp
+++ solutions/medium/Permutations.cpp
@@ -3,32 +3,35 @@
             // Language: C++
             // Link: https://leetcode.com/problems/permutations/
 
-private:
+                skip (continue)\n";
+    }
+
     void backtrack(vector<int>& nums, vector<int>& curr, vector<bool>& visited){
 
-        if(curr.size()==nums.size()){
-            res.push_back(curr);   
+        printState("ENTER backtrack, depth=" + to_string(depth), curr, visited);
+        depth++; // going one level deeper for everything inside this call
+
+        if(curr.size() == nums.size()){
+            cout << string(depth * 2, ' ') << ">>> BASE CASE HIT -> push [";
+            for(int x : curr) cout << x << " ";
+            cout << "]\n";
+            res.push_back(curr);
+            depth--; // returning, so step back out
             return;
         }
 
-        for(int i=0;i<nums.size();i++){
-            if(visited[i]==1)continue;
-            curr.push_back(nums[i]);
-            visited[i]=1;
-            backtrack(nums,curr,visited);
-            visited[i]=0;
-            curr.pop_back();
-        }                       
-    }
-
-public:
-    vector<vector<int>>res;
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> curr;
-        vector<bool> visited(nums.size(),0);
-        backtrack(nums,curr,visited);
-        return res;
-    }
-};
-
+        for(int i = 0; i < nums.size(); i++){
+            if(visited[i] == 1){
+                cout << string(depth * 2, ' ') << "i=" << i << " already visited -> 
 class Solution {
+private:
+    int depth = 0; // tracks recursion depth, just for indentation
+
+    // helper to print curr[] and visited[] at any point, indented by depth
+    void printState(const string& label, vector<int>& curr, vector<bool>& visited){
+        cout << string(depth * 2, ' ') << label
+             << " | curr=[";
+        for(int x : curr) cout << x << " ";
+        cout << "] visited=[";
+        for(bool b : visited) cout << b << " ";
+        cout << "]\n";
