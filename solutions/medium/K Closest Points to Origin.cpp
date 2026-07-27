d792235c71704dd81a9a04c1a064683877c261b4
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-07-27 08:26:04 +0530

    Add solution for K Closest Points to Origin

---
+++ solutions/medium/K Closest Points to Origin.cpp
@@ -0,0 +1,6 @@
+// Title: K Closest Points to Origin
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/k-closest-points-to-origin/
+
+c
