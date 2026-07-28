054c219af810317531ffc18887aa684639a51dc8
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-07-28 09:58:55 +0530

    Add solution for Kth Largest Element in an Array

--- solutions/medium/Kth Largest Element in an Array.cpp
+++ solutions/medium/Kth Largest Element in an Array.cpp
@@ -3,4 +3,34 @@
             // Language: C++
             // Link: https://leetcode.com/problems/kth-largest-element-in-an-array/
 
-c
+        int pivot = nums[rand() % nums.size()];
+        
+        vector<int> left;
+        vector<int> mid;
+        vector<int> right;
+        
+        for (int num: nums) {
+            if (num > pivot) {
+                left.push_back(num);
+            } else if (num < pivot) {
+                right.push_back(num);
+            } else {
+                mid.push_back(num);
+            }
+        }
+        
+        if (k <= left.size()) {
+            return quickSelect(left, k);
+        }
+        
+        if (left.size() + mid.size() < k) {
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+        
+        return pivot;
+        
+    }
+};
+    int quickSelect(vector<int>& nums, int k) {
+    
+    }
