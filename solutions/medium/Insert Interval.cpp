9836c0b59f2c22b32276d52fdd7eab0870862874
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-08-01 09:34:20 +0530

    Add solution for Insert Interval

--- solutions/medium/Insert Interval.cpp
+++ solutions/medium/Insert Interval.cpp
@@ -3,18 +3,21 @@
             // Language: C++
             // Link: https://leetcode.com/problems/insert-interval/
 
-            intervals.insert(intervals.begin(), newInterval);
-            return intervals;
-        }
-        if (newInterval[0] > intervals[intervals.size() - 1][1]) {
-            intervals.insert(intervals.end(), newInterval);
+                               vector<int>& newInterval) {
+        if(intervals.empty()){
+            intervals.push_back(newInterval);
             return intervals;
         }
         int i = 0;
         while (i < intervals.size()) {
             if (newInterval[0] <= intervals[i][1]) {
                 if(newInterval[1]<intervals[i][0]){
-                while (j+1< intervals.size() && newInterval[1] >= intervals[j+1][0])
+                    intervals.insert(intervals.begin()+i, newInterval);
+                    return intervals;
+                }
+                int j = i;
+                while (j+1< intervals.size() && newInterval[1] >= intervals[j+1]
+                [0])
                     j++;
                 vector<int> merge = {min(intervals[i][0], newInterval[0]),
                                      max(intervals[j][1], newInterval[1])};
@@ -22,12 +25,12 @@
                 intervals.insert(intervals.begin() + i, merge);
                 return intervals;
             }
-                int j = i;
-                    intervals.insert(intervals.begin()+i, newInterval);
-                }
-                    return intervals;
             else i++;
         }
+    intervals.insert(intervals.end(), newInterval);
     return intervals;
     }
 };
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+public:
+class Solution {
