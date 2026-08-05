7c3f1f9810564820c85bcb2056df657a13a45676
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-08-05 19:27:40 +0530

    Add solution for Meeting Rooms II

--- solutions/medium/Meeting Rooms II.cpp
+++ solutions/medium/Meeting Rooms II.cpp
@@ -3,4 +3,27 @@
             // Language: C++
             // Link: https://leetcode.com/problems/meeting-rooms-ii/
 
-c
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if(intervals.empty())return 0;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        pq.push(intervals[0][1]);
+
+        size_t rooms=1;
+        for(int i=1;i<intervals.size();i++){
+
+            if(intervals[i][0]>=pq.top()){
+                pq.pop();
+                pq.push(intervals[i][1]);
+            }
+            else{
+               pq.push(intervals[i][1]); 
+            }
+            rooms=max(rooms,pq.size());
+        }
+    return rooms;
+    }
+        
+        sort(intervals.begin(),intervals.end());
+};
