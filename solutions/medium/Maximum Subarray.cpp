cb758728e54a0201af3edc6896f4edfcdfb126c3
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-08-07 11:05:12 +0530

    Add solution for Maximum Subarray

--- solutions/medium/Maximum Subarray.cpp
+++ solutions/medium/Maximum Subarray.cpp
@@ -3,22 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/maximum-subarray/
 
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int i=0;
-        int j=0;
-        int curr_sum=nums[0];
-        int max_sum=curr_sum;
-        while(j<nums.size()-1){
-            j++;
-            curr_sum+=nums[j];
-            while(i<j && (nums[i]<0 || curr_sum<0)){
-                curr_sum-=nums[i];
-                i++;      
-            }
-            max_sum=max(max_sum,curr_sum);
-        }
-    return max_sum;
-    }
-};
+c
