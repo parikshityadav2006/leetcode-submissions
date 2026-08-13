6f88f31ba063c32fec58abe6e4e62e50c220990a
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-08-13 10:06:18 +0530

    Add solution for Clone Graph

--- solutions/medium/Clone Graph.cpp
+++ solutions/medium/Clone Graph.cpp
@@ -3,36 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/clone-graph/
 
-            if(mp.find(curr)==mp.end()){
-                NewNode = new Node(curr->val);
-                mp[curr]=NewNode;
-            }
-            else NewNode=mp[curr];
-            
-            for(Node* i : curr->neighbors) {
-                if(visited.count(i)==0){
-                    q.push(i);
-                    visited.insert(i);
-                }
-                Node* neighbor;
-                if(mp.find(i)==mp.end()){
-                    neighbor = new Node(i->val);
-                    mp[i]=neighbor;
-            Node* NewNode;
-            q.pop();
-            Node* curr = q.front();
-        while (!q.empty()) {
-        bool flag=1;   //using flag to update root node
-        
-        Node* root=nullptr;   //to store deep copy of first node
-        
-        visited.insert(node);
-        q.push(node);   //initialise q with first node
-        queue<Node*> q;
-        unordered_map<Node*,Node*> mp;   //to interconnect existing nodes
-        unordered_set<Node*> visited;
-                }
-                else neighbor=mp[i];
-                
-                NewNode->neighbors.push_back(neighbor);
-            }
+ 
