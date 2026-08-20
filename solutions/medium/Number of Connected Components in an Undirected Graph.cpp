441795739dee36e5c67312e9b63bce5281947bf4
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-08-20 14:13:00 +0530

    Add solution for Number of Connected Components in an Undirected Graph

--- solutions/medium/Number of Connected Components in an Undirected Graph.cpp
+++ solutions/medium/Number of Connected Components in an Undirected Graph.cpp
@@ -3,35 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
 
-    }
-        if(rank[rx]<rank[ry]) swap(rx,ry);
-        if(rx==ry) return false;
-        parent[ry]=rx;
-        rank[rx]++;
-        return true;
-        components--;
-
-    bool connected(int x,int y){
-        return find(x)==find(y);
-    }
-
-    int numComponents(){
-        return components;
-    }
-};
-class Solution {
-public:
-    int countComponents(int n, vector<vector<int>>& edges) {
-        UnionFind graphs(n);
-
-        for(auto& edge: edges){
-            graphs.unite(edge[0],edge[1]);
-        }
-
-        int ry=find(y);
-        int rx=find(x);
-    bool unite(int x, int y){
-
-    }
-        return find(parent[node]);
-        if(parent[node]==node) return node;
+c
