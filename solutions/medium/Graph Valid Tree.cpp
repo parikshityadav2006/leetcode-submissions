ab054d212f100628244e13f0a12d594c62d30c82
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-08-20 19:14:23 +0530

    Add solution for Graph Valid Tree

--- solutions/medium/Graph Valid Tree.cpp
+++ solutions/medium/Graph Valid Tree.cpp
@@ -3,34 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/graph-valid-tree/
 
-        
-        // Make the adjacency list.
-        for (auto& edge : edges) {
-            adjacencyList[edge[0]].push_back(edge[1]);
-            adjacencyList[edge[1]].push_back(edge[0]);
-        }
-        
-        if ((int)edges.size() != n - 1) return false;
-public:
-    vector<vector<int>> adjacencyList;
-        
-
-        if (seen.count(node)) return;
-        seen.insert(node);
-        for (int neighbour : adjacencyList[node]) {
-            dfs(neighbour);
-        }
-    }
-        // Carry out depth first search.
-        dfs(0);
-        // Inspect result and return the verdict.
-        return (int)seen.size() == n;
-    }
-    
-    unordered_set<int> seen;
-    bool validTree(int n, vector<vector<int>>& edges) {
-    void dfs(int node) {
-private:
-class Solution {
-        adjacencyList.resize(n);
-};
+c
