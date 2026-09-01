45a4cbf5df40686536b91ea58729b67df4288649
parikshityadav2006 <parikshityadav2025@gmail.com>
2026-09-01 17:25:14 +0530

    Add solution for Cheapest Flights Within K Stops

--- solutions/medium/Cheapest Flights Within K Stops.cpp
+++ solutions/medium/Cheapest Flights Within K Stops.cpp
@@ -3,4 +3,34 @@
             // Language: C++
             // Link: https://leetcode.com/problems/cheapest-flights-within-k-stops/
 
- 
+    int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& e : flights) {
+            adj[e[0]].push_back({e[1], e[2]});
+        }
+        vector<int> dist(n, INT_MAX);
+        queue<pair<int, int>> q;
+        q.push({src, 0});
+        int stops = 0;
+
+        while (stops <= k && !q.empty()) {
+            int sz = q.size();
+            // Iterate on current level.
+            for(int i=0; i<sz; i++) {
+                auto [node, distance] = q.front();
+                q.pop();
+                // Iterate over neighbors of popped node.
+                for (auto& [neighbour, price] : adj[node]) {
+                    if (price + distance >= dist[neighbour]) continue;
+                    dist[neighbour] = price + distance;
+                    q.push({neighbour, dist[neighbour]});
+                }
+            }
+            stops++;
+        }
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
+    }
+};
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, 
+class Solution {
+public:
